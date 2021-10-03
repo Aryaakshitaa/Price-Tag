6e9fc04564db304b68ca36e82aa1e61ebf378fa6
Aryaakshitaa <45949378+Aryaakshitaa@users.noreply.github.com>
2021-10-03 04:22:14 -0700

    Update pricingCard.component.ts

--- src/app/pricingCard/pricingCard.component.ts
+++ src/app/pricingCard/pricingCard.component.ts
@@ -6,7 +6,50 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./pricingCard.component.css']
 })
 export class PricingCardComponent implements OnInit {
-  @Input() myData: any = {};
+  @Input() myData = {
+    title: "Free",
+    price: "$0",
+    user: {
+      data: "Single User",
+      available: true,
+      bold: false
+    },
+    storage: {
+      data: "5GB Storage",
+      available: true,
+      bold: false
+    },
+    projectA: {
+      data: "Unlimited Public Projects",
+      available: true,
+      bold: false
+    },
+    access: {
+      data: "Community Access",
+      available: true,
+      bold: false
+    },
+    projectB: {
+      data: "Unlimited Private Projects",
+      available: false,
+      bold: false
+    },
+    support: {
+      data: "Dedicated Phone Support",
+      available: false,
+      bold: false
+    },
+    subdomain: {
+      data: "Free Subdomain",
+      available: false,
+      bold: false
+    },
+    status: {
+      data: "Monthly Status Reports",
+      available: false,
+      bold: false
+    }
+  };
 
   constructor() { }
 
